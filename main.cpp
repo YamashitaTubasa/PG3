f76f684693ce91e664c25f167263cce3671a5a24
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-11-14 09:22:04 +0900

    1-3.関数ポインタとコールバック関数

--- main.cpp
+++ main.cpp
@@ -1,29 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
 
-// 一般的な賃金体系の計算
-int GeneralWage(int general,int t) {
-	return general * t;
+typedef void(*PFunc)(int*);
+
+void DiceResult(int* n) {
+	srand(time(nullptr));
+	int dice = rand() % 5 + 1;
+
+	if (dice % 2 == *n) {
+		printf("アタリ\n");
+	}
+	else {
+		printf("ハズレ\n");
+	}
+
+	printf("サイコロの結果 → %d\n", dice);
 }
 
-// 再帰的な賃金体系の計算
-int RecursiveWage(int recursive,int n) {
-	if (n == 1) {
-		return recursive;
-	}
-	return (RecursiveWage(recursive,n - 1) * 2 - 50);
+void SetTimeout(PFunc p, int secound, int number) {
+	Sleep(secound);
+	p(&number);
 }
 
-int main() {
-	int general = 1072;      // 一般的な賃金体系
-	int recursive = 100;     // 再帰的な賃金体系
-	int recursiveTotal = 0;  // 再帰的な賃金体系の合計
-	const int time = 15;     // 時間
+int main(void) {
+	PFunc p;
 
-	for (int i = 1; i < time; i++) {
-		recursiveTotal += RecursiveWage(recursive,i);
-		printf("一般的な賃金体系[%d時間 - %d円]\n", i, GeneralWage(general,i));
-		printf("再帰的な賃金体系[%d時間 - %d円]\n\n", i, recursiveTotal);
-	}
+	int number = 0;     // 番号
+	int secound = 3000; // 秒数
+
+	printf("半(奇数)だと思ったら [ 1 ] 、 丁(偶数)だと思ったら [ 0 ]を入力してください\n↓\n");
+	scanf_s("[ %d ]", &number);
+
+	p = DiceResult;
+	SetTimeout(p, secound, number);
 
 	return 0;
 }
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <windows.h>
+//#include <time.h>
+//
+//typedef void (*PFunc)(int*);
+//
+//void diceResult(int* num) {
+//
+//    srand(time(nullptr));
+//    int dice = rand() % 5 + 1;
+//
+//    if (dice % 2 == *num) {
+//        printf("当たり\n");
+//    }
+//    else {
+//        printf("はずれ\n");
+//    }
+//
+//    printf("さいころの出目は%dでした\n", dice);
+//
+//}
+//
+//void setTimeout(PFunc p, int second, int num) {
+//
+//    //コールバック関数を呼び出す
+//    Sleep(second);
+//
+//    //macやUnix系OSの場合
+//    //sleep(second);
+//    p(&num);
+//
+//}
+//
+//int main(void) {
+//
+//    PFunc p;
+//
+//    int number = 0;
+//    int second = 3000;
+//
+//    printf("半(奇数)なら[1],丁(偶数)なら[0]を入力してください\n");
+//    scanf_s("%d\n", &number);
+//
+//    p = diceResult;
+//
+//    setTimeout(p, second, number);
+//
+//    return 0;
+//
+//}

7546c8ac88feedf0eb0b500103e18f9def89f5b9
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-11-28 09:36:46 +0900

    0

--- main.cpp
+++ main.cpp
@@ -1,40 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-#include <time.h>
 #include <iostream>
 #include <functional>
+#include <list>
 
 int main(void) {
 
-	int number = 0;   // 番号
-	int waitTime = 3; // 答えが出るまで待ちの時間
-
-	srand(time(nullptr));
-	int dice = rand() % 6 + 1;
-
-	// サイコロの抽選関数
-	std::function<void()> diceDrawing = [=]() {
-		if (dice % 2 == number) {
-			printf("アタリ\n");
-		}
-		else {
-			printf("ハズレ\n");
-		}
+	const int JY = 30;
+	char stationName[JY][30] = {
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Nishi-Nippori",
+		"Tabara",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Takanawa Gateway",
+		"Tamachi"
 	};
 
-	// 待ち時間の関数
-	std::function<void(std::function<void()>, int)> SetTimeout = [](std::function<void()> fx, int waitTime) { 
-		Sleep(waitTime * 1000); fx(); 
-	};
-	
-	printf("半(奇数)だと思ったら [ 1 ] 、 丁(偶数)だと思ったら [ 0 ]を入力してください\n↓\n");
-	
-	scanf_s("[ %d ]", &number); // プレイヤーから奇数か偶数の入力受付
-
-	SetTimeout(diceDrawing, waitTime);
+	std::list<int> list;
+	std::list<int>::iterator itr;
 
 	return 0;
-
 }
 

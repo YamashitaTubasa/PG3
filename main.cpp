c315cdfe89dcd65a85785fb3cdb591c16015d03a
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-12-02 21:36:03 +0900

    単方向リストを自作(char型を使用)

--- main.cpp
+++ main.cpp
@@ -3,45 +3,61 @@
 #include <string.h>
 
 typedef struct cell
-{
-	char str[8];
+{   
+	char str[10];
 	struct cell *next;
 }CELL;
 
 void create(CELL *endCell, const char *buf); // ƒZƒ‹‚ðV‹Kì¬‚·‚éŠÖ”
 void index(CELL *endCell); // ƒZƒ‹‚Ìˆê——‚ð•\Ž¦‚·‚éŠÖ”
+void deleteElement(CELL* endCell); // ÅŒã‚Ì—v‘f‚ðíœ‚·‚éŠÖ”
 
 int main(void) {
 
-	int num;
-	char str[8];
+	int selectionNum; // ‘I‘ð”Ô†
+	char str[10];
 	
 	CELL head;
 	head.next = nullptr;
 
-	printf("[—v‘f‚Ì‘€ì]\n1.—v‘f‚Ìˆê——•\Ž¦\n2.ÅŒã”ö‚É—v‘f‚Ì‘}“ü\n3.ÅŒã”ö‚Ì—v‘f‚Ìíœ\n\n");
-	printf("--------------------------------\n‘€ì‚ð‘I‘ð‚µ‚Ä‚­‚¾‚³‚¢\n");
+	printf("[—v‘f‚Ì‘€ì]\n");
+	printf("1.—v‘f‚Ìˆê——•\Ž¦\n");
+	printf("2.ÅŒã”ö‚É—v‘f‚Ì‘}“ü\n");
+	printf("3.ÅŒã”ö‚Ì—v‘f‚Ìíœ\n\n");
+	printf("--------------------------------\n");
+	printf("‘€ì‚ð‘I‘ð‚µ‚Ä‚­‚¾‚³‚¢\n");
 
 	while (true) {
-		scanf_s("%d", &num);
+		scanf_s("%d", &selectionNum);
 		printf("\n");
 
-		if (num == 2) {
-			printf("[ƒŠƒXƒg—v‘f‚Ì‘}“ü]\n\n’Ç‰Á‚·‚é—v‘f‚Ì’l‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n“ü—Í‚·‚é’l : ");
-			scanf_s("%s", str, 8);
-			printf("\n—v‘f[%s]‚ªƒŠƒXƒg‚ÌÅŒã”ö‚É‘}“ü‚³‚ê‚Ü‚µ‚½\n\n", str);
-			printf("----------------------------------------\n0.‰Šú‰æ–Ê‚É–ß‚é\n");
+		if (selectionNum == 0) {
+			printf("[—v‘f‚Ì‘€ì]\n");
+			printf("1.—v‘f‚Ìˆê——•\Ž¦\n");
+			printf("2.ÅŒã”ö‚É—v‘f‚Ì‘}“ü\n");
+			printf("3.ÅŒã”ö‚Ì—v‘f‚Ìíœ\n\n");
+			printf("--------------------------------\n");
+			printf("‘€ì‚ð‘I‘ð‚µ‚Ä‚­‚¾‚³‚¢\n");
 		}
 
-		create(&head, str);
+		if (selectionNum == 2) {
+			printf("[ƒŠƒXƒg—v‘f‚Ì‘}“ü]\n\n");
+			printf("’Ç‰Á‚·‚é—v‘f‚Ì’l‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+			printf("“ü—Í‚·‚é’l : ");
+			scanf_s("%s", str, 10);
+			printf("\n—v‘f[ %s ]‚ªƒŠƒXƒg‚ÌÅŒã”ö‚É‘}“ü‚³‚ê‚Ü‚µ‚½\n\n", str);
+			printf("--------------------------------\n");
+			printf("0.‰Šú‰æ–Ê‚É–ß‚é\n");
+			create(&head, str);
+		}
 
-		if (num == 1) {
+		if (selectionNum == 1) {
 			index(&head);
 		}
 
-		if (num == 0) {
-			printf("[—v‘f‚Ì‘€ì]\n1.—v‘f‚Ìˆê——•\Ž¦\n2.ÅŒã”ö‚É—v‘f‚Ì‘}“ü\n3.ÅŒã”ö‚Ì—v‘f‚Ìíœ\n\n");
-			printf("--------------------------------\n‘€ì‚ð‘I‘ð‚µ‚Ä‚­‚¾‚³‚¢\n");
+
+		if (selectionNum == 3) {
+			deleteElement(&head);
 		}
 	}
 
@@ -52,9 +68,9 @@ void create(CELL *endCell, const char *buf) {
 	CELL* newCell;
 
 	newCell = (CELL*)malloc(sizeof(CELL));
-
-	strcpy_s(newCell->str, 8, buf);
-	//newCell->str = buf;
+	
+	strcpy_s(newCell->str, 10, buf);
+	
 	newCell->next = nullptr;
 
 	while (endCell->next != nullptr) 
@@ -62,17 +78,38 @@ void create(CELL *endCell, const char *buf) {
 		endCell = endCell->next;
 	}
 
-	endCell = newCell;
+	endCell->next = newCell;
 }
 
 void index(CELL *endCell) {
-	printf("[—v‘f‚Ìˆê——]\n—v‘fˆê——:{\n");
+	int elementNum = -1; // —v‘f”Ô†
+	printf("[—v‘f‚Ìˆê——]\n");
+	printf("—v‘fˆê——:{\n");
 
 	while (endCell->next != nullptr) 
 	{
+		elementNum++;
 		endCell = endCell->next;
-		printf("%s,\n", endCell->str);
+		printf("%d: %s,\n", elementNum, endCell->str);
 	}
 
 	printf("}\n\n\n");
+	printf("—v‘f”: %d\n", elementNum + 1);
+	printf("---------------------------------\n");
+	printf("0.‰Šú‰æ–Ê‚É–ß‚é\n");
+}
+
+void deleteElement(CELL* endCell)
+{
+	CELL *prevCell;
+	prevCell = endCell->next;
+
+	printf("[—v‘f‚Ìíœ]\n");
+	printf("ÅŒã”ö‚Ì—v‘f‚ðíœ‚µ‚Ü‚µ‚½\n\n\n");
+	printf("---------------------------------\n");
+	printf("0.‰Šú‰æ–Ê‚É–ß‚é\n");
+	
+	if (prevCell->next != nullptr) {
+		prevCell->next = nullptr;
+	}
 }
